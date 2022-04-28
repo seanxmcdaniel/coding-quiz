b5e95246fbcfa4fc654102aa7f16c54122b8b58b
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-04-28 12:18:39 -0400

    navigating

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,31 @@ const question1 = document.querySelector(".questions");
 const quiz_end = document.querySelector(".quiz-end");
 const cont_btn = document.querySelector(".cont-btn");
 
-start_quiz.addEventListener('click', () => {
+start_quiz.addEventListener("click", () => {
 if(info.style.display === 'none') {
     info.style.display = 'flex';
+    start_quiz.style.display = 'none';
 } else {
     info.style.display = 'none';
 }
 });
 
+const to_q_1 = document.querySelector("#to-q-1");
+
+to_q_1.addEventListener("click", () => {
+if(question1.style.display === 'none') {
+    question1.style.display = 'flex';
+    info.style.display = 'none';
+} else {
+    question1.style.display = 'none';
+}
+});
+
+
 var timerEl = document.getElementById('timer');
 
 function countdown() {
     var timeLeft = 60;
-  
     
     var timeInterval = setInterval(function () {
       if (timeLeft > 1) {
